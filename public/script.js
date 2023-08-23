78095948dd010577fa8fee4cf0bc7840705aa1a0
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-23 19:00:08 -0400

    Update and rename dashboard-script.js to script.js

---
+++ public/script.js
@@ -0,0 +1,42 @@
+// script.js
+
+// Netlify Identity Logic
+if (document.getElementById('loginButton')) {
+    document.getElementById('loginButton').addEventListener('click', function() {
+        netlifyIdentity.open('login');
+    });
+}
+
+netlifyIdentity.on('login', user => {
+    console.log('login', user);
+    window.location.href = "dashboard.html"; 
+});
+
+netlifyIdentity.on('logout', () => {
+    console.log('Logged out');
+    window.location.href = "login.html"; 
+});
+
+function logout() {
+    console.log('Logout function called');
+    netlifyIdentity.logout();
+}
+
+// Cloudinary Widget Logic
+const widget = cloudinary.createUploadWidget(
+    {
+        cloudName: 'laxdotcom',
+        uploadPreset: 'prevectorlogos'
+    },
+    (error, result) => {
+        if (!error && result && result.event === "success") {
+            console.log('Done! Here is the image info: ', result.info);
+            document.getElementById(result.info.field).value = result.info.secure_url;
+        }
+    }
+);
+
+function uploadImage(field) {
+    widget.open();
+    widget.update({ 'field': field });
+}
