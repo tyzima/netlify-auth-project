ec4535d94c93853c5538617f168dd5aef246ed8a
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-23 23:10:23 -0400

    Update addToAirtable.js

--- netlify/functions/addToAirtable.js
+++ netlify/functions/addToAirtable.js
@@ -1,38 +1,45 @@
 
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: '${AIRTABLE_API_KEY}'}).base('appRB6D5YhUGSE9QA');
 
-base('ProductRequests').create([
-  {
-    "fields": {
-      "1": "1",
-      "SalesRep": "NL",
-      "Date": "2021-06-23",
-      "Does this item exist on Lax.com?": "Yes",
-      "Gender": "Male",
-      "Is this a sku modification?": "No",
-      "Accessories ": "Shoulder pads",
-      "Vendor": "Maverik ",
-      "Brand": "Maverik ",
-      "Vendor Item Code/Sku": "Mav-MxEKGSP",
-      "Vendor Item Name": "Maverik MX EKG Shoulder Pad",
-      "Sizes - (XS-XL)": "S-XL",
-      "Colors - (White-100)": "Grey",
-      "Wholesale Price": 42.5,
-      "Retail Price": 84.99,
-      "Netsuite / Salesforce Item Code": "Mav-MxEKGSP-GY"
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
     }
-  }
- 
-], function(err, records) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  records.forEach(function (record) {
-    console.log(record.getId());
-  });
-});
 
+    const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME } = process.env;
+    const URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
 
+    let parsedBody;
 
+    try {
+        parsedBody = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: "Invalid JSON body"
+        };
+    }
+
+    const salesRep = parsedBody.salesRep;
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+            "Content-Type": "application/json"
+        }
+    };
+
+    try {
+        const response = await axios.post(URL, { fields: { "SalesRep": salesRep } }, config); // Only sending the salesRep field
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to add to Airtable" })
+        };
+    }
+};
