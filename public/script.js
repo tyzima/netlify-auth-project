e9b5663688f7ff88ebcfb0c0fc2c9aa9ba03cacb
tyzima <41972512+tyzima@users.noreply.github.com>
2023-08-23 21:31:33 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -55,3 +55,38 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+$('#productForm').on('submit', function(event) {
+    event.preventDefault();
+
+    const data = {
+        salesRep: $('#salesRep').val(),
+        date: $('#date').val(),
+        itemExistence: $('#itemExistenceToggle').prop('checked') ? 'Yes' : 'No',
+        gender: $('#gender').val(),
+        skuModification: $('#skuModificationToggle').prop('checked') ? 'Yes' : 'No',
+        accessories: $('#accessories').val(),
+        vendor: $('#vendor').val(),
+        brand: $('#brand').val(),
+        vendorItemCode: $('#vendorItemCode').val(),
+        vendorItemName: $('#vendorItemName').val(),
+        sizes: $('#sizes').val(),
+        colors: $('#colors').val(),
+        wholesalePrice: $('#wholesalePrice').val(),
+        retailPrice: $('#retailPrice').val(),
+        netsuiteItemCode: $('#netsuiteItemCode').val(),
+        oneField: $('#oneField').val(),
+        notes: $('#notes').val(),
+        frontView: $('#frontView').val(),
+        backView: $('#backView').val()
+    };
+
+    $.post('/.netlify/functions/addToAirtable', { data: data }, function(response) {
+        if (response && response.id) {
+            // Data added successfully
+            alert("Data added to Airtable!");
+        } else {
+            alert("Error adding data to Airtable");
+        }
+    });
+});
+
